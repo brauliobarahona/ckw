8cf72b14fe85e86e7158d449a99451d4058f3680
braulio <braulio.barahonagarzon@hslu.ch>
2024-02-23 00:10:02 +0100

    first version to parse and unzip

---
+++ parse.py
@@ -0,0 +1,88 @@
+"""
+  Parse CKW open data hosted at https://open.data.axpo.com
+
+  There are two datasets:
+    **Datensatz A** : dataset a, anonymized data
+    **Datensatz B** : dataset b, aggregated data
+
+  This scripts does the following:
+    1. Download data from website
+    2. Unzip data
+    3. Parse data to a compressed format
+
+  The file names can be parse from the website or a text file, see metatada.txt
+
+  TODO: forrmat of file names is given, just create file name with user input
+          YYYYMM.csv.gzs
+"""
+import os
+from bs4 import BeautifulSoup
+import requests
+
+# use beautiful soup to parse the website and extract the download links  
+url_da = "https://open.data.axpo.com/%24web/index.html#dataset-a"
+
+# get html content
+html_content = requests.get(url_da).text # string
+
+# parse html content, extract body from html content text
+soup = BeautifulSoup(html_content, features="html.parser")
+html_table = soup.find_all("table") # ResultSet object : dataset a (anonymized), dataset b (aggregated)
+
+# extract tr from element tag of html_table containing file names of dataset a
+tr_list = html_table[0].find_all("tr") 
+
+# find in bs4.element.ResultSet
+file_names = []
+for tr_i in tr_list:
+  fn = tr_i.find_all("th")[0].text
+  
+  if fn.endswith(".csv.gz"):
+    file_names.append(fn)
+
+    file_size = tr_i.find_all("td")[2].text
+    date = tr_i.find_all("td")[1].text
+    #link = tr_i.find_all("td")[-1].contents # link
+  else:
+    continue
+
+# construct download url
+_prefix = "https://open.data.axpo.com/%24web/"
+
+# get file name from user input or from a list
+#   fn0 = "ckw_opendata_smartmeter_dataset_a_202308.csv.gz" # for download
+#   url0 = "https://open.data.axpo.com/%24web/ckw_opendata_smartmeter_dataset_a_202308.csv.gz"
+
+""" download a batch of files
+  1. given a local directory
+  2. check if files exist
+  3. download files from the website
+"""
+# check if files exist
+downloads = "/Users/tabaraho/ckw" # *.gz and *.csv files
+for fi in file_names:
+  fn = os.path.join(downloads, fi)
+  
+  if os.path.exists(fn):
+    print("This file exisits: {}".format(fn))
+
+  elif not os.path.exists(fn):
+    # download file and save it to downloads
+    url_i = _prefix + fi
+    curl_cmd = f"curl -o {fn} {url_i}"
+
+    print(curl_cmd)
+    #os.system(curl_cmd)
+  print(fn)
+  
+# Unzip files
+for fi in file_names:
+  fn = os.path.join(downloads, fi)
+  fout = os.path.join(downloads, fi.replace(".gz", ""))
+  if os.path.exists(fn):
+    print("This file exists: {}".format(fn))
+    
+    # unzip file
+    unzip_cmd = f"gunzip -c {fn} > {fout}"
+    print(unzip_cmd)
+    #os.system(unzip_cmd)
